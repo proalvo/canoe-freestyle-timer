effb5d6ac5672e0aef8d4cdaf836d6f1ef369363
proalvo <k_nykanen@hotmail.com>
2021-09-07 09:36:25 +0300

    Add files via upload

--- canoe-freestyle-timer.ino
+++ canoe-freestyle-timer.ino
@@ -2,14 +2,16 @@
 
 Timer for Freestyle Canoe
 Author: Kari Nykänen k_nykanen@hotmail.com
-Date: 14/09/2020
+
 Version: 1.6, output to serial port has been updated
+20/10/2020 Version: 1.7, start the timer if "S"tart is received from the serial port 
+02/04/2020 Version: 1.8, added support for the rotary switch to change the competition time 
 
 Complies with rules for surface boats:
 ICF Canoe Freestyle Competition Rules 2019
 https://www.canoeicf.com/sites/default/files/rules_canoe_freestyle_2019.pdf
 
-Arduino Uno pin connecyions:
+Arduino Uno digital pin connections:
 
 Pin 2 ---- |------ 10kohm --- GND   (Brown)
            |
@@ -19,13 +21,20 @@ Pin 3 ---- (+) end buzzer (Velleman VM411) (-)--------- GND
 
 Pin 4 ---- (+ Red (XLR 1) ) start buzzer (Velleman VM411) (- Green (XLR 4)) ------ GND  
 
+Pin 5 --- (not in use)
+
+Led Matrix Display:
 Pin 6 --> LMD pin 2 (A)
 Pin 7 --> LMD pin 4 (B)
 Pin 8 --> LMD pin 10 (SCLK)
 Pin 9 --> LMD pin 1 (OE)
 Pin 11 --> LMD pin 12 (DATA)
 Pin GND --> LMD pin 3,5,7,9,11,13,15 (GND)
- 
+
+ROTARY SWITCH
+Pin A0 ---> rotary switch, common pin
+5V --> Rotary swith pin 4 --|1.2Kohm|--pin3---|1.2Kohm|--pin2--|1.2Kohm|--pin1--|330ohm|--GND
+
  */
 
 #include <SPI.h>
@@ -34,23 +43,21 @@ Pin GND --> LMD pin 3,5,7,9,11,13,15 (GND)
 #include <fonts/Timerx32x32.h>
 
 
-#define DISPLAYS_ACROSS 1
-#define DISPLAYS_DOWN 2
+#define DISPLAYS_ACROSS 1  // number of DMD panels sideways
+#define DISPLAYS_DOWN 2    // number of DMD panels vertically
 
 SoftDMD dmd(DISPLAYS_ACROSS,DISPLAYS_DOWN);  // DMD controls the entire display
 
-
 //global variables available anywhere in the program
 
-unsigned long startMillis;  
-unsigned long currentMillis;
-unsigned long timeElapsed;
-
+unsigned long startMillis;    // value of millis() at the time of start
+unsigned long currentMillis;  // value of millis() at the moment
+unsigned long timeElapsed;    // time elapsed since start
 
 long buttonTimer = 0;
 long longPressTime = 100; // time needed to press the start button, 1000 = 1 second, 100 = 100 ms
 
-unsigned long elapsedTime = 0;
+unsigned long elapsedTime = 0;  // obsolete?
 unsigned long pastTime = 0;
 
 unsigned long RUN_TIME = 45000;         // rule 5.1.4, time of competition run in milliseconds - default value
@@ -61,32 +68,79 @@ boolean playTone = false;
 boolean playToneOK = false; 
 boolean playStartBuzzer = false;
 boolean buttonActive = false;
-boolean longPressActive = false;
+boolean longPressActive = false;  // obsolete?
 
 const int buttonPin = 2;          // pushbutton pin 
 const int buzzerPin = 3;          // end buzzer pin 3
 const int buzzerStartPin = 4;     // start buzzer pin 4
 
+int switchValue = 0;  // rotary switch, v1.8
+
 int buttonState = 0; 
 
 // variables for different modes of operation
 
-const int MODE_READY = 0;   // ready to start
-const int MODE_RUN = 1;     // running the competition
-const int MODE_END_1 = 2;   // play the end tone 
-const int MODE_END_2 = 3;   // 
-
+const int MODE_READY  = 0;   // ready to start
+const int MODE_RUN    = 1;     // running the competition
+const int MODE_END_1  = 2;   // play the end tone 
+const int MODE_END_2  = 3;   // 
 int mode;  
+
 // int fromTime = 45;
 
 //char temp[15]; // tämä on testausta varten 9.9.2020
+
 char serialCharTime[4]; 
 String serialStringTime;
 boolean x = false;
 
-// the setup routine runs once when you press reset:
+int incomingByte = 0; // ver 1.7
 
+/* ***********************************************************************************************
+// routine to setup the competition time
+**************************************************************************************************/
+
+
+void selectTime()
+{
+  if (mode == MODE_READY) { // allow change of time with rotary switch only if mode is MODE_READY i.e. competition is not running
+
+    switchValue = analogRead(A0);  
+    if ( switchValue > 900 ) {
+      RUN_TIME = 45000;         
+      WARNING_TIME = 35000;
+    } 
+
+    else if ( switchValue < 900 && switchValue > 550 ) {
+      RUN_TIME = 60000;         
+      WARNING_TIME = 50000;
+    }
+
+    else if ( switchValue < 550 && switchValue > 200 ) {
+      RUN_TIME = 45000;         
+      WARNING_TIME = 35000;
+    }
+
+    else if ( switchValue < 550 && switchValue > 200 ) {
+      RUN_TIME = 45000;         
+      WARNING_TIME = 35000;
+    } 
+
+    else {
+      RUN_TIME = 45000;         
+      WARNING_TIME = 35000;
+    }
+             
+  }
+  return;
+}
+
+
+/* ***********************************************************************************************
+// the setup routine runs once when you start or reset the Arduino 
+**************************************************************************************************/
 void setup() {
+  
   Serial.begin(38400);  //start Serial in case we need to print debugging info
   Serial.write("OK\n");  // send OK to serial port
   
@@ -97,14 +151,26 @@ void setup() {
   pinMode(buzzerPin, OUTPUT);       // initialize the buzzer pin as an output
   pinMode(buzzerStartPin, OUTPUT);  // initialize the start buzzer pin as an output
   pinMode(buttonPin, INPUT);        // initialize the pushbutton pin as an input
-  
+
+  // setup for the analog input / rotary swith
+  pinMode(A0, INPUT);
+
+  mode = MODE_READY;
+   
   currentMillis = RUN_TIME;         // 
 
-  dmd.drawString(1,0,String(RUN_TIME/1000));  // draw start  time to the LED screen
+  // dmd.drawString(1,0,String(RUN_TIME/1000));  // draw start  time to the LED screen
 
   delay(1000);  
-  Serial.write("  \n");  // send empty string to serial port, used toe clean the "OK"
+  Serial.write("  \n");  // send empty string to serial port, used to clean the "OK"
+
+  selectTime();
+  
+  dmd.drawString(1,0,String(RUN_TIME/1000));
 }
+
+
+
 
 /* ***********************************************************************************************
 // the loop routine runs over and over again forever:
@@ -112,46 +178,35 @@ void setup() {
 
 void loop() {
 
-  if (digitalRead(buttonPin) == HIGH) { // #buttonPressed if START button has been pressed ...
+   /* ***********************************************************************************************
+   // routine to read the switch
+   **************************************************************************************************/
+  
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+  }
+  
+  if (digitalRead(buttonPin) == HIGH  || incomingByte == 83) { // #buttonPressed if START button has been pressed or S received from serial port ...
+    incomingByte = 0;
 
-    if (buttonActive == false) {    // if button has not been pressed earlier ..
+      /* ***********************************************************************************************
+      // routine to start the competition if the switch has been pressed
+      **************************************************************************************************/ 
+      if (mode == MODE_READY || mode == MODE_END_2 ) {  // if mode is READY or Xs are blingking in the end of run
 
-      buttonActive = true;          // then set it active 
-      buttonTimer = millis();       // and start to counter
-
-    }
-
-    if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
-
-      longPressActive = true;                 // button has beem pressed for the required time 
-
-      if (mode == MODE_READY || mode == MODE_END_2 ) {  // if mode is READY
-
-        startMillis = millis()-longPressTime; // start time is when key was pressed ( current time - when key was pressed
-        elapsedTime = 0;                      
+        startMillis = millis();//-longPressTime; // start time is when key was pressed ( current time - when key was pressed
+        elapsedTime = 0;      // obsolete?                
         mode = MODE_RUN;                      // set mode to RUN = start the run
         serialStringTime = String(RUN_TIME/1000-elapsedTime)+"\n";  // convert time to be suitable for serial port
         serialStringTime.toCharArray(serialCharTime,4);
         Serial.write(serialCharTime);
 
-        
+            
         dmd.selectFont(Timer32);              // set font
-        digitalWrite(buzzerStartPin,HIGH);    // play sound to start the run, here is the minor bug, it comes wiht delay.
+        digitalWrite(buzzerStartPin,HIGH);    // play sound to start the run 
         playStartBuzzer = true;
       }
-
-    }
-
-  } else {   // end of #buttonPressed
-
-    if (buttonActive == true) {
-
-      if (longPressActive == true) {
-        longPressActive = false;
-      }
-      buttonActive = false;
-
-    }
 
   }
 
@@ -181,18 +236,12 @@ void loop() {
             if ( (RUN_TIME/1000-elapsedTime) <10 ) {  // if time is less than 10 seconds..
               dmd.clearScreen();
               dmd.drawString(9,0,String(RUN_TIME/1000-elapsedTime)); // then write number to the middle of screen
-              serialStringTime = String(RUN_TIME/1000-elapsedTime) + "\n";  // convert time to be suitable for serial port
-              serialStringTime.toCharArray(serialCharTime,4);
-              Serial.write(serialCharTime);
             }
             else {  
               dmd.clearScreen();
               dmd.drawString(1,0,String(RUN_TIME/1000-elapsedTime));   // write number from left to right              
-              serialStringTime = String(RUN_TIME/1000-elapsedTime) + "\n";  // convert time to be suitable for serial port
-              serialStringTime.toCharArray(serialCharTime,4);
-              Serial.write(serialCharTime);
             }
-
+            Serial.println(RUN_TIME/1000-elapsedTime);
             pastTime = elapsedTime;            
          }
 
@@ -221,7 +270,7 @@ void loop() {
 
 // play end tones, which is two signals, 
 // beep (1 sec) - silence (1 sec) - beeb (1 sec)
-// total time 3 seconds and you can not interrupt it by pressing start button
+// total time 3 seconds and you cannot interrupt it by pressing the start switch
 
   if ( mode == MODE_END_1 ) {
     digitalWrite(buzzerPin,HIGH);  // start playing the tone
